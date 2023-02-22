07674e3a9642a1e2a7896df2007c175db8a98317
YPKCos <84114948+YPKCos@users.noreply.github.com>
2023-02-22 16:46:48 +0800

    add define function

---
+++ main.c
@@ -0,0 +1,19 @@
+// Statement expressions make these macros side-effect safe
+#define MIN_8bits(a, b)                                                        \
+  ({                                                                           \
+    typeof(a) _a = (a);                                                        \
+    typeof(b) _b = (b);                                                        \
+    _a < _b ? (_a & 0xFF) : (_b & 0xFF);                                       \
+  })
+#define MIN(a, b)                                                              \
+  ({                                                                           \
+    typeof(a) _a = (a);                                                        \
+    typeof(b) _b = (b);                                                        \
+    _a < _b ? _a : _b;                                                         \
+  })
+#define MAX(a, b)                                                              \
+  ({                                                                           \
+    typeof(a) _a = (a);                                                        \
+    typeof(b) _b = (b);                                                        \
+    _a > _b ? _a : _b;                                                         \
+  })
